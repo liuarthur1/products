18550125810d187e0ccabafb3906d3688b62bd7c
liuarthur1 <57953071+liuarthur1@users.noreply.github.com>
2020-01-27 22:01:30 +0800

    read files

--- products.py
+++ products.py
@@ -1,5 +1,11 @@
 
 products = []
+with open('products.csv','r', encoding='utf-8') as f:
+	for line in f:
+		name, price = line.strip().split(',')
+		print([name, price])
+		products.append([name, price])
+
 while True:
 	name = input("input products Names:")
 	if name == 'q':
@@ -15,6 +21,6 @@ for pp in products:
 	print(pp[0], 'price is:', pp[1])
 
 with open('products.csv','w', encoding='utf-8') as f:
-	f.write('商品,price\n')
+	#f.write('商品,price\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + "\n")
