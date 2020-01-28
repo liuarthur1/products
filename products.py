9304530d26092a2c12c94296cabaff0de912794e
liuarthur1 <57953071+liuarthur1@users.noreply.github.com>
2020-01-28 14:31:43 +0800

    refactor

--- products.py
+++ products.py
@@ -1,26 +1,57 @@
+import os
 
-products = []
-with open('products.csv','r', encoding='utf-8') as f:
-	for line in f:
-		name, price = line.strip().split(',')
-		print([name, price])
+# Read File
+def read_file(filename):
+    products = []
+    with open(filename,'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,price' in line:
+                continue
+            name, price = line.strip().split(',')
+            print([name, price])
+            products.append([name, price])
+    return products
+
+
+# Print Products
+def print_products(products):
+	for product in products:
+		print(product)
+
+
+
+
+# User input
+def input_product(products):
+	while True:
+		name = input("input products Names:")
+		if name == 'q':
+			break
+		price = input("Input the price:")
+		# p = []
+		# p.append(name)
+		# p.append(price)
 		products.append([name, price])
+	return products
 
-while True:
-	name = input("input products Names:")
-	if name == 'q':
-		break
-	price = input("Input the price:")
-	# p = []
-	# p.append(name)
-	# p.append(price)
-	products.append([name, price])
-print(products)
+ 
+# Write File
+def write_file(products,filename):
+    with open(filename,'w', encoding='utf-8') as f:
+        f.write('商品,price\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + "\n")
 
-for pp in products:
-	print(pp[0], 'price is:', pp[1])
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('yeah! ')
+		products = read_file(filename)
+	#print(products)
+	else:
+		print('File not found!!!')
+	products = input_product(products)
+	print_products(products)
+	write_file(products,filename)
 
-with open('products.csv','w', encoding='utf-8') as f:
-	#f.write('商品,price\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + "\n")
+main()
